9d8f18f3340cb7a24e7b4270ab12f462bf1c9d81
Nutsu31 <116025262+Nutsu31@users.noreply.github.com>
2023-05-31 15:45:42 +0400

    lets start

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+import "./styles/app.css";
+function App() {
+  return (
+    <Box sx={{ margin: 0, padding: 0, boxSizing: "border-box", width: "100%" }}>
+      sdad
+    </Box>
+  );
+}
+
+export default App;
